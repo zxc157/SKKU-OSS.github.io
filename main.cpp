d7fa1ea88334966bbfff5d0d629398f47effc26f
Jhj9 skkusosc <iris99hj@naver.com>
2019-12-09 12:43:48 +0900

    Added printMain function
    Signed-off-by: Jhj9 skkusosc <iris99hj@naver.com>

--- main.cpp
+++ main.cpp
@@ -13,23 +13,38 @@ struct Student {
     double gpa;
 };
 
+void printMain(){
+    cout << "\n [Main Menu]\n";
+    cout << "===========================\n";
+    cout << "  1. Add Student\n";
+    cout << "  2. Print Students\n";
+    //cout << "  2. Delete Student\n";
+    //cout << "  3. Edit Student\n";
+    //cout << "  4. Search Student\n";
+    //cout << "  5. Group Assignment\n";
+    cout << "  0. Exit\n";
+    return;
+};
+
 int main(){
     char a, c;
     int studentNum = 0;
     Student s[100];
     while (true) {
+	printMain();
 	cin >> c;
 	switch (c) {
 	    case '0':{
 		exit(0);
 	    }
-	    case '1': {
+	    case '1': {	
+		break;
 	    }
 	    case '2': {
+		break;
 	    }	
-	}
-	default:
-	    break;
+	    default:
+	        break;
 	}
     }
 }
